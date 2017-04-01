b590c0712358d6d7bc128222984b7a79fca05edd
Mike Ledger <mike@quasimal.com>
2017-04-01 14:48:45 +1100

    use postgres

--- src/Api.hs
+++ src/Api.hs
@@ -12,6 +12,7 @@
 -- $ stack ghc -- teamsplit.hs -O2 -threaded
 -- $ ./teamsplit http://localhost > api.js
 {-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE DeriveAnyClass             #-}
 {-# LANGUAGE DeriveGeneric              #-}
 {-# LANGUAGE ExistentialQuantification  #-}
 {-# LANGUAGE FlexibleInstances          #-}
@@ -24,17 +25,24 @@
 {-# LANGUAGE TemplateHaskell            #-}
 {-# LANGUAGE TypeFamilies               #-}
 {-# LANGUAGE TypeOperators              #-}
-module Api (start, js) where
+module Api where
 import           Conduit
+import qualified Control.Lens                         as L
+import           Control.Monad                        (void)
 import           Control.Monad.IO.Class               (liftIO)
 import           Control.Monad.Logger
+import qualified Data.Aeson.TH                        as JSON
+import           Data.Char
 import           Data.Int
 import           Data.Proxy
 import           Data.Text                            (Text)
+import qualified Data.Text                            as T
+import qualified Data.Text.Encoding                   as T
 import           Data.Time
 import           Database.Esqueleto
-import           Database.Persist.Sqlite              (ConnectionPool,
-                                                       createSqlitePool,
+import qualified Database.Persist                     as P
+import           Database.Persist.Postgresql          (ConnectionPool,
+                                                       createPostgresqlPool,
                                                        runMigration,
                                                        runSqlPersistMPool,
                                                        runSqlPool)
@@ -46,6 +54,8 @@ import qualified Network.Wai.Handler.Warp             as Warp (runEnv)
 import           Network.Wai.Middleware.RequestLogger
 import           Servant
 import           Servant.JS
+import           System.Environment
+import           Web.Heroku.Postgres                  (dbConnParams)
 
 type Player = Text
 type MapType = Text
@@ -61,19 +71,33 @@ mapTypes =
 
 --------------------------------------------------------------------------------
 
+type Rate = (UTCTime, Word)
+
+data Rating = Rating
+  { ratingTable :: Text
+  , ratingWho   :: Text
+  , ratingDate  :: UTCTime
+  , ratingMap   :: Text
+  , ratingElo   :: Word
+  } deriving (Generic)
+
+JSON.deriveToJSON JSON.defaultOptions
+  { JSON.fieldLabelModifier = L.over L._head toLower . drop 6 }
+  ''Rating
+
 -- db shit
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
-Rating json
-  date UTCTime
+Ratings json
   table Text
   map Text
   who Text
-  elo Word
-  caveat Text Maybe
+  rate Rate
+  pastRates [Rate]
+  UniqueRatings table who map
   deriving Show Eq
   |]
 
-deriving instance Generic (Key Rating) -- ugly hack
+deriving instance Generic (Key Ratings) -- ugly hack
 type SqliteM = SqlPersistT (NoLoggingT (ResourceT IO))
 
 {-# SPECIALISE pageSize :: Int #-}
@@ -85,56 +109,71 @@ getTables :: Int64 -> SqliteM [Text]
 getTables page =
   map unValue <$>
   (select $ from $ \rating -> do
-      orderBy [desc (rating^.RatingDate)]
+      orderBy [desc (rating^.RatingsTable)]
+      groupBy (rating^.RatingsTable)
       offset (pageSize * page)
       limit pageSize
-      groupBy (rating^.RatingTable)
-      return (rating^.RatingTable))
+      return (rating^.RatingsTable))
 
 getRatings :: Text -> SqliteM [Rating]
 getRatings table =
-  map entityVal <$> (
+  map (currentRating . entityVal) <$> (
   select $ from $ \rating -> do
-    where_ (rating^.RatingTable ==. val table)
-    orderBy [desc (rating^.RatingDate)]
-    groupBy (rating^.RatingWho)
+    where_ (rating^.RatingsTable ==. val table)
     return rating)
 
-getAllRatings :: Text -> SqliteM [Rating]
-getAllRatings table =
-  map entityVal <$> (
-  select $ from $ \rating -> do
-    where_ (rating^.RatingTable ==. val table)
-    orderBy [desc (rating^.RatingDate)]
-    return rating)
+currentRating :: Ratings -> Rating
+currentRating Ratings{..} = Rating
+  { ratingTable = ratingsTable
+  , ratingMap = ratingsMap
+  , ratingWho = ratingsWho
+  , ratingElo = snd ratingsRate
+  , ratingDate = fst ratingsRate
+  }
+
+allRatings :: Ratings -> [Rating]
+allRatings r@Ratings{..} = currentRating r : map
+  (\(date, elo) -> Rating
+    { ratingTable = ratingsTable
+    , ratingMap = ratingsMap
+    , ratingWho = ratingsWho
+    , ratingElo = elo
+    , ratingDate = date })
+  ratingsPastRates
 
 ratingHistory :: Text -> Text -> SqliteM [Rating]
 ratingHistory table who =
-  map entityVal <$> (
-  select $ from $ \rating -> do
-    where_ (rating^.RatingTable ==. val table &&. rating^.RatingWho ==. val who)
-    orderBy [asc (rating^.RatingDate)]
-    return rating)
+  concatMap (allRatings . entityVal) <$> (
+  select $ from $ \ratings -> do
+  where_ (ratings^.RatingsTable ==. val table &&.
+          ratings^.RatingsWho ==. val who)
+  return ratings)
 
 getPlayers :: Int64 -> SqliteM [Text]
-getPlayers page =
-  map unValue <$>
-  select (distinct (from (\r -> do
-    offset (pageSize * page)
-    limit pageSize
-    return (r^.RatingWho))))
+getPlayers page = map unValue <$> select (distinct (from (\r -> do
+  offset (pageSize * page)
+  limit pageSize
+  return (r^.RatingsWho))))
 
-setRating :: Text -> Player -> MapType -> Word -> Maybe Text -> SqliteM ()
-setRating table player maptype elo caveat = do
+setRatings :: Text -> Player -> MapType -> Word -> Maybe Text -> SqliteM ()
+setRatings table player maptype elo _caveat = do
   time <- liftIO getCurrentTime
-  insert_ Rating
-    { ratingDate = time
-    , ratingTable = table
-    , ratingMap = maptype
-    , ratingWho = player
-    , ratingElo = elo
-    , ratingCaveat = caveat
-    }
+  mrs  <- P.getBy (UniqueRatings table player maptype)
+  case mrs of
+    Just (Entity k rs) -> void (replace k Ratings
+      { ratingsTable = table
+      , ratingsMap = maptype
+      , ratingsWho = player
+      , ratingsRate = (time, elo)
+      , ratingsPastRates = ratingsRate rs : ratingsPastRates rs
+      })
+    Nothing -> insert_ Ratings
+      { ratingsTable = table
+      , ratingsMap = maptype
+      , ratingsWho = player
+      , ratingsRate = (time, elo)
+      , ratingsPastRates = []
+      }
 
 --------------------------------------------------------------------------------
 
@@ -168,16 +207,20 @@ server pool =
   :<|> return mapTypes
   :<|> (\table player -> sql (ratingHistory table player))
   :<|> (\table player maptype elo caveat -> do
-           sql (setRating table player maptype elo caveat)
+           sql (setRatings table player maptype elo caveat)
            return True)
   :<|> sql . getRatings
   where
     sql :: SqlPersistT (NoLoggingT (ResourceT IO)) a -> Handler a
     sql f = liftIO (runSqlPersistMPool f pool)
 
-makeApplication :: IO Application
-makeApplication = do
-  pool <- runStderrLoggingT (createSqlitePool "teamsplit.sqlite" 10)
+-- host=localhost port=5432 user=test dbname=test password=test
+makeConnStr :: [(Text, Text)] -> Text
+makeConnStr params = T.unwords [ T.concat [k, "=", v] | (k, v) <- params ]
+
+makeApplication :: [(Text, Text)] -> IO Application
+makeApplication params = do
+  pool <- runStderrLoggingT (createPostgresqlPool (T.encodeUtf8 (makeConnStr params)) 10)
   runSqlPool (runMigration migrateAll) pool
   return (serve api (server pool :<|> serveDirectory "dist"))
 
@@ -190,4 +233,16 @@ js = jsForAPI
       })
 
 start :: IO ()
-start = Warp.runEnv 80 . logStdoutDev =<< makeApplication
+start = do
+  params <- do
+    url <- lookupEnv "DATABASE_URL"
+    case url of
+      Just _ -> dbConnParams
+      Nothing -> return
+        [ ("host", "localhost")
+        , ("port", "5432")
+        , ("user", "postgres")
+        , ("dbname", "teamsplat")
+        , ("password", "")
+        ]
+  Warp.runEnv 80 . logStdoutDev =<< makeApplication params
