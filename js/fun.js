ef8e0d3941ee756c750c2298f1fc65680aaa3e06
Mike Ledger <mike@quasimal.com>
2017-05-15 20:39:49 +1000

    better fun

--- js/fun.js
+++ js/fun.js
@@ -13,19 +13,24 @@ window.onfocus = () => {
 
 window.onblur = () => {
   inactive = true;
+  havingFun = false;
+  funzone.innerHTML = '';
 };
 
 const songs = {
   lotto: 'https://www.youtube.com/embed/n52tm3kjqgQ',
-  electric: 'https://www.youtube.com/embed/w323VoKky1Q',
+//  electric: 'https://www.youtube.com/embed/w323VoKky1Q',
   dollDance: 'https://www.youtube.com/embed/yiWgQ1b4SsM',
   discoShuffle: 'https://www.youtube.com/embed/NGggrr89VnA',
   shaft2: 'https://www.youtube.com/embed/FS8K9bTr_jc',
+  lotto2: 'https://www.youtube.com/embed/n52tm3kjqgQ',
   quicksand: 'https://www.youtube.com/embed/pIHWJSIgRQI',
   eightyeight: 'https://www.youtube.com/embed/hn7WRJJnYJ0',
   sicilian: 'https://www.youtube.com/embed/XUFJfco3vBY',
   adentro: 'https://www.youtube.com/embed/DqtcjI1_k2o',
-  duncow: 'https://www.youtube.com/embed/7DDAuytA_YY',
+  lotto3: 'https://www.youtube.com/embed/n52tm3kjqgQ',
+//  duncow: 'https://www.youtube.com/embed/7DDAuytA_YY',
+  // lotto is great so make it 3x as likely :-)
 };
 
 const names: string[] = keys(songs);
@@ -41,7 +46,7 @@ function fun(youtube) {
   if (funzone && !havingFun) {
     havingFun = true;
     funzone.innerHTML = (
-      `<iframe width="560" height="315" src="${youtube}?autoplay=1" frameborder="0"></iframe>`
+      `<iframe id="yt" width="560" height="315" src="${youtube}?autoplay=1&enablejsapi=1" frameborder="0"></iframe>`
     );
     setTimeout(() => {
       funzone.style.display = 'none';
@@ -52,7 +57,7 @@ function fun(youtube) {
 setInterval(() => {
   if (inactive) {
     window.funcounter += 1;
-    console.log('inactive', window.funcounter);
+    // console.log('inactive', window.funcounter);
   }
   if (window.funcounter > 720) {
     fun(choose());
