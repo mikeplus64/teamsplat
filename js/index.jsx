c666d8970502d74de95066fbafb18efc41d51eda
Mike Ledger <mike@quasimal.com>
2017-05-23 12:53:52 +1000

    never have fun again

--- js/index.jsx
+++ js/index.jsx
@@ -4,14 +4,12 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, Route, hashHistory } from 'react-router';
 import '../node_modules/grommet/scss/vanilla/index.scss';
-
 import App from './components/App.jsx';
 import Editor from './components/Editor.jsx';
 import Tables from './components/Tables.jsx';
 import TableControls from './components/TableControls.jsx';
 import NewTable from './components/NewTable.jsx';
 import connect from './store';
-import './fun.js';
 
 render(connect(store =>
   <Provider store={store}>
